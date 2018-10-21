97e9024462cd9bc25fc0de6a477808d69735d703
lilei-ncut/netcoredemo.git <baqiaoll007@163.com>
2018-10-21 16:20:35 +0800

    Init

---
+++ QuickStarts/1_ClientCredentials/src/Client/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+namespace Client
+{
+    class Program
+    {
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        private static async Task MainAsync()
+        {
+            // 从元数据中发现端口
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+            // 1. 客户端授权模式-- 请求令牌 
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+            Console.WriteLine(tokenResponse.Json);
+
+            // 调用api 
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+            var response = await client.GetAsync("http://localhost:64100/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+
+            //2. 资源所有者密码授权模式-- 请求令牌
+            var tokenClientResource = new TokenClient(disco.TokenEndpoint, "ro.client", "secret");
+            var tokenResponseResource = await tokenClientResource.RequestResourceOwnerPasswordAsync("alice", "password", "api1");//使用用户名密码
+            if (tokenResponseResource.IsError)
+            {
+                Console.WriteLine(tokenResponseResource.Error);
+                return;
+            }
+            Console.WriteLine(tokenResponseResource.Json);
+
+            // 调用api 
+            client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+            response = await client.GetAsync("http://localhost:64100/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+    }
+}
---
+++ QuickStarts/1_ClientCredentials/src/QuickstartIdentityServer/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace QuickstartIdentityServer
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // 使用内存存储的密钥，客户端和API资源来配置ids4。
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryApiResources(Config.GetApiResources()) // 添加api资源
+                .AddInMemoryClients(Config.GetClients()) // 添加客户端 
+                .AddTestUsers(Config.GetUsers()); //添加测试用户
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+
+            loggerFactory.AddConsole(LogLevel.Debug);
+            app.UseDeveloperExceptionPage();
+
+            app.UseIdentityServer();
+        }
+    }
+}
